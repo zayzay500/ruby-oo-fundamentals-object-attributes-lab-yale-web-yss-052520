b1e1873a133b742d66a2023e798117fc59ca25ae
zayzay500 <lindsay.pierce@yale.edu>
2020-05-25 19:57:41 -0400

    Done.

---
+++ lib/dog.rb
@@ -0,0 +1,19 @@
+class Dog
+    # dog name getter/setter methods
+    def name=(dog_name)
+        @name = dog_name
+    end
+
+    def name
+        @name
+    end
+
+    #dog breed getter/setter methods
+    def breed=(breed_name)
+        @breed = breed_name
+    end
+
+    def breed
+        @breed
+    end
+end
